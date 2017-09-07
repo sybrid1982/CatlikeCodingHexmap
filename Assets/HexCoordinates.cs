75c37585a999897e070bd96f3566f66a6c24312f
Sydney Bridges <sybrid@gmail.com>
2017-09-07 17:02:57 -0400

    Added Y to hexCoordinates Y is calculated as -x-z because that's how the math works out

--- Assets/HexCoordinates.cs
+++ Assets/HexCoordinates.cs
@@ -19,11 +19,11 @@ public struct HexCoordinates  {
 
     public override string ToString()
     {
-        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+        return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
     }
 
     public string ToStringOnSeparateLines ()
     {
-        return X.ToString() + "\n" + Z.ToString();
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
     }
 }
