65866c4aa29ff4d14b7ac79a017d8ec37d31dfc7
Sydney Bridges <sybrid@gmail.com>
2017-09-07 17:01:36 -0400

    Added HexCoordinates HexCoordinates take the offset mess of the actual coordinates of the cells and turns them into much happier axial coordinates

---
+++ Assets/HexCoordinates.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates  {
+    public int X { get; private set; }
+    public int Z { get; private set; }
+    public int Y { get { return -X - Z; } }
+
+    public HexCoordinates (int x, int z)
+    {
+        X = x;
+        Z = z;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates (int x, int z)
+    {
+        return new HexCoordinates(x - z /2, z);
+    }
+
+    public override string ToString()
+    {
+        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+    }
+
+    public string ToStringOnSeparateLines ()
+    {
+        return X.ToString() + "\n" + Z.ToString();
+    }
+}
