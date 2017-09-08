a872da20f31a590de6fffa3589cc926818010ecf
Sydney Bridges <sybrid@gmail.com>
2017-09-08 10:09:01 -0400

    Grid Coordinates Visible in Editor Created a custom property drawer to show the coordinates for any given hex in the editor.

--- Assets/HexCoordinates.cs
+++ Assets/HexCoordinates.cs
@@ -2,14 +2,17 @@
 
 [System.Serializable]
 public struct HexCoordinates  {
-    public int X { get; private set; }
-    public int Z { get; private set; }
+    [SerializeField]
+    private int x, z;
+
+    public int X { get { return x; } }
+    public int Z { get { return z; } }
     public int Y { get { return -X - Z; } }
 
     public HexCoordinates (int x, int z)
     {
-        X = x;
-        Z = z;
+        this.x = x;
+        this.z = z;
     }
 
     public static HexCoordinates FromOffsetCoordinates (int x, int z)
